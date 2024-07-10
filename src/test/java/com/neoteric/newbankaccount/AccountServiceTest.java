4039eefc7e65e434818c7a95061578d6d55e8ecd
Kurapati Rakesh <rakewshkurapati.18@gmail.com>
2024-07-10 10:14:48 +0530

    Bankaccount

--- src/test/java/com/neoteric/newbankaccount/AccountServiceTest.java
+++ src/test/java/com/neoteric/newbankaccount/AccountServiceTest.java
@@ -6,8 +6,6 @@ import com.neoteric.newbankaccount.service.AccountService;
 import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
-//import #java service method test
-//import sun.jvm.hotspot.debugger.Address;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.UUID;
